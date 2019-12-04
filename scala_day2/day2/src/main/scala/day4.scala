3d33baf130b80df15b7650bbfd89a7a272f12a6f
mdesgroseilliers <marc.desgroseilliers@upc.ch>
2019-12-04 11:12:55 +0100

    day 4 solution

--- scala_day2/day2/src/main/scala/day4.scala
+++ scala_day2/day2/src/main/scala/day4.scala
@@ -1,12 +1,23 @@
 object day4 {
     def check1(num:Int):Boolean={
-        val pairs = num.toString.map(_.asDigit).sliding(3).toList
+        val pairs = num.toString.map(_.asDigit).sliding(2).toList
         val temp = (for {pair <- pairs}
-            yield (check3(pair), (pair(0)<=pair(1)) && (pair(1)<=pair(2)))).unzip
-        temp._1.exists(identity) && temp._2.forall(identity)
+            yield (pair(0)==pair(1), pair(0)<=pair(1))).unzip
+        val consecutive = temp._1.sliding(3).toList//.map(e=>e(0).^(e(1))).toList
+
+        (consecutive.map(e=>checkC(e)).exists(identity) || checkFirst(consecutive.head) || checkLast(consecutive.last)) && temp._2.forall(identity)
     }
-    def check3(triple:IndexedSeq[Int]):Boolean={
-        if (triple(0)==triple(1) && triple(1)==triple(2)) false else triple(0)==triple(1) || triple(1)==triple(2)
+
+    def checkC(triple:List[Boolean]):Boolean={
+        triple==List(false, true, false)
+    }
+
+    def checkFirst(triple:List[Boolean]):Boolean={
+        triple(0)==true && triple(1)==false
+    }
+
+    def checkLast(triple:List[Boolean]):Boolean={
+        triple(1)==false && triple(2)==true
     }
     val result = for(i<-(234208 to 765869) if check1(i)) yield i
 
